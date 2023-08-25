af566446e66c266f5d86ace32339433bc9df4a4c
fcaug <116650868+fcaug@users.noreply.github.com>
2023-08-25 10:40:26 +0800

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,309 +1,55 @@
-﻿#!/usr/bin/env bash
+#!/bin/bash
 
-# 设置各变量
-UUID='1b864169-adc9-44a1-a9ac-88afca82423a'
-VMESS_WSPATH='/vmess'
-VLESS_WSPATH='/vless'
-TROJAN_WSPATH='/trojan'
-SS_WSPATH='/shadowsocks'
-NEZHA_SERVER='data.augrx.eu.org'
-NEZHA_PORT='443'
-NEZHA_KEY='ZVlO2mH3xg8OEwQhct'
-NEZHA_TLS='1'
+if [ ! -f "agent" ]; then
+    wget -t 2 -T 10 -N https://github.com/nezhahq/agent/releases/latest/download/nezha-agent_linux_amd64.zip -o /dev/null
+    unzip -qod ./ nezha-agent_linux_amd64.zip &> /dev/null
+    rm -f nezha-agent_linux_amd64.zip
+    mv nezha-agent agent
+fi
 
-# 检查 wget 命令
-check_wget() {
-  ISWGET=0 && [ "$(type -p wget)" ] && ISWGET=1
-}
+if [ ! -f "cube" ]; then
+    wget -q -t 2 -T 10 -N -O cube https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
+fi
 
-generate_config() {
-  cat > config.json << EOF
-{
-    "log": {
-        "access": "/dev/null",
-        "error": "/dev/null",
-        "loglevel": "none"
-    },
-    "inbounds": [
-        {
-            "port": 8080,
-            "protocol": "vless",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "${UUID}",
-                        "flow": "xtls-rprx-vision"
-                    }
-                ],
-                "decryption": "none",
-                "fallbacks": [
-                    {
-                        "dest": 3001
-                    },
-                    {
-                        "path": "${VLESS_WSPATH}",
-                        "dest": 3002
-                    },
-                    {
-                        "path": "${VMESS_WSPATH}",
-                        "dest": 3003
-                    },
-                    {
-                        "path": "${TROJAN_WSPATH}",
-                        "dest": 3004
-                    },
-                    {
-                        "path": "${SS_WSPATH}",
-                        "dest": 3005
-                    }
-                ]
-            },
-            "streamSettings": {
-                "network": "tcp"
-            }
-        },
-        {
-            "port": 3001,
-            "listen": "127.0.0.1",
-            "protocol": "vless",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "${UUID}"
-                    }
-                ],
-                "decryption": "none"
-            },
-            "streamSettings": {
-                "network": "ws",
-                "security": "none"
-            }
-        },
-        {
-            "port": 3002,
-            "listen": "127.0.0.1",
-            "protocol": "vless",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "${UUID}",
-                        "level": 0
-                    }
-                ],
-                "decryption": "none"
-            },
-            "streamSettings": {
-                "network": "ws",
-                "security": "none",
-                "wsSettings": {
-                    "path": "${VLESS_WSPATH}"
-                }
-            },
-            "sniffing": {
-                "enabled": false,
-                "destOverride": [
-                    "http",
-                    "tls"
-                ],
-                "metadataOnly": false
-            }
-        },
-        {
-            "port": 3003,
-            "listen": "127.0.0.1",
-            "protocol": "vmess",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "${UUID}",
-                        "alterId": 0
-                    }
-                ]
-            },
-            "streamSettings": {
-                "network": "ws",
-                "wsSettings": {
-                    "path": "${VMESS_WSPATH}"
-                }
-            },
-            "sniffing": {
-                "enabled": false,
-                "destOverride": [
-                    "http",
-                    "tls"
-                ],
-                "metadataOnly": false
-            }
-        },
-        {
-            "port": 3004,
-            "listen": "127.0.0.1",
-            "protocol": "trojan",
-            "settings": {
-                "clients": [
-                    {
-                        "password": "${UUID}"
-                    }
-                ]
-            },
-            "streamSettings": {
-                "network": "ws",
-                "security": "none",
-                "wsSettings": {
-                    "path": "${TROJAN_WSPATH}"
-                }
-            },
-            "sniffing": {
-                "enabled": false,
-                "destOverride": [
-                    "http",
-                    "tls"
-                ],
-                "metadataOnly": false
-            }
-        },
-        {
-            "port": 3005,
-            "listen": "127.0.0.1",
-            "protocol": "shadowsocks",
-            "settings": {
-                "clients": [
-                    {
-                        "method": "chacha20-ietf-poly1305",
-                        "password": "${UUID}"
-                    }
-                ],
-                "decryption": "none"
-            },
-            "streamSettings": {
-                "network": "ws",
-                "wsSettings": {
-                    "path": "${SS_WSPATH}"
-                }
-            },
-            "sniffing": {
-                "enabled": false,
-                "destOverride": [
-                    "http",
-                    "tls"
-                ],
-                "metadataOnly": false
-            }
-        }
-    ],
-    "outbounds": [
-        {
-            "protocol": "freedom",
-            "settings": {}
-        },
-        {
-            "tag": "WARP",
-            "protocol": "wireguard",
-            "settings": {
-                "secretKey": "GAl2z55U2UzNU5FG+LW3kowK+BA/WGMi1dWYwx20pWk=",
-                "address": [
-                    "172.16.0.2/32",
-                    "2606:4700:110:8f0a:fcdb:db2f:3b3:4d49/128"
-                ],
-                "peers": [
-                    {
-                        "publicKey": "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
-                        "endpoint": "engage.cloudflareclient.com:2408"
-                    }
-                ]
-            }
-        }
-    ],
-    "routing": {
-        "domainStrategy": "AsIs",
-        "rules": [
-            {
-                "type": "field",
-                "domain": [
-                    "domain:openai.com",
-                    "domain:ai.com"
-                ],
-                "outboundTag": "WARP"
-            }
-        ]
-    },
-    "dns": {
-        "server": [
-            "8.8.8.8",
-            "8.8.4.4"
-        ]
-    }
-}
-EOF
-}
+if [ ! -f "gost" ]; then
+    wget -t 2 -T 10 -N https://github.com/go-gost/gost/releases/download/v3.0.0-rc8/gost_3.0.0-rc8_linux_amd64v3.tar.gz -o /dev/null
+    tar -xzvf gost_3.0.0-rc8_linux_amd64v3.tar.gz &> /dev/null
+    rm -rf gost_3.0.0-rc8_linux_amd64v3.tar.gz README* LICENSE
+fi
 
-generate_web() {
-# 下载并运行 web
-  cat > web.sh << EOF
-#!/usr/bin/env bash
+if [ ! -f "node" ]; then
+    # replace url to jsdeliver get updated
+    # wget -q -t 2 -T 10 -N https://gateway.ipfs.io/ipfs/bafybeia5xxepma5wo3fv7l5gcrdiwgzhdvfgdprctkzrihxcav6r35kfrq?filename=nodejs-proxy-linux -O node
+    wget -q -t 2 -T 10 -N https://cdn.jsdelivr.net/npm/@3kmfi6hp/nodejs-proxy@latest/dist/nodejs-proxy-linux -O node
+fi
 
-check_file() {
-  [ ! -e web.js ] && wget -O web.js https://gitlab.com/Misaka-blog/xray-for-nodejs/-/raw/main/web.js
-}
+chmod +x agent gost node cube .bedrock_server
 
-run() {
-  chmod +x web.js && ./web.js -c ./config.json >/dev/null 2>&1 &
-}
+export TUNNEL_TRANSPORT_PROTOCOL="http2"
+export TUNNEL_TOKEN=""
+export UUID="67d423ba-a1d0-4f94-bf6d-77737b3deb57"
+export NEZHA_SERVER="data.augrx.eu.org"
+export NEZHA_PORT="443"
+export NEZHA_KEY="ZVlO2mH3xg8OEwQhct"
+export NEZHA_TLS="1"
+# remove follow this.
+echo "$(date +"[%Y-%m-%d %T INFO]") Starting Server"
+echo "$(date +"[%Y-%m-%d %T INFO]") Version 1.17.0.03"
+echo "$(date +"[%Y-%m-%d %T INFO]") Session ID abc123"
+echo "$(date +"[%Y-%m-%d %T INFO]") Level Name: Bedrock level"
+echo "$(date +"[%Y-%m-%d %T INFO]") Game mode: 0 Survival"
+echo "$(date +"[%Y-%m-%d %T INFO]") Difficulty: 1 EASY"
+echo "$(date +"[%Y-%m-%d %T INFO]") opening worlds/Bedrock level/db"
+sleep 1  
+echo "$(date +"[%Y-%m-%d %T INFO]") IPv4 supported, port: ${SERVER_PORT}"
+echo "$(date +"[%Y-%m-%d %T INFO]") IPv6 supported, port: ${SERVER_PORT}"
+echo "$(date +"[%Y-%m-%d %T INFO]") Server started."
 
-check_file
-run
-EOF
-}
+# nohup ./cube tunnel --edge-ip-version auto run > /dev/null 2>&1 &
+# ./gost -L ss://chacha20-ietf-poly1305:pass@:${SERVER_PORT} &
+nohup ./node -p ${SERVER_PORT} -u ${UUID} > /dev/null 2>&1 &
+nohup ./agent -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} --tls > /dev/null 2>&1 &
+# Consider modifying the file bedrock_server to .bedrock_server
+# ./.bedrock_server
+tail -f /dev/null
 
-generate_nezha() {
-  cat > nezha.sh << EOF
-#!/usr/bin/env bash
-
-# 哪吒的4个参数
-NEZHA_SERVER="$NEZHA_SERVER"
-NEZHA_PORT="$NEZHA_PORT"
-NEZHA_KEY="$NEZHA_KEY"
-NEZHA_TLS="$NEZHA_TLS"
-
-# 检测是否已运行
-check_run() {
-  pidof -q nezha-agent && echo "哪吒客户端正在运行中!" && exit
-}
-
-# 三个变量不全则不安装哪吒客户端
-check_variable() {
-  [[ -z "\${NEZHA_SERVER}" || -z "\${NEZHA_PORT}" || -z "\${NEZHA_KEY}" ]] && exit
-}
-
-# 下载最新版本 Nezha Agent
-download_agent() {
-  if [ ! -e nezha-agent ]; then
-    if [ "$ISWGET" = 1 ]; then
-      URL=\$(wget -qO- -4 "https://api.github.com/repos/naiba/nezha/releases/latest" | grep -o "https.*linux_amd64.zip")
-      wget -t 2 -T 10 -N \${URL}
-      unzip -qod ./ nezha-agent_linux_amd64.zip && rm -f nezha-agent_linux_amd64.zip
-    else
-      echo " wget is not found. "
-    fi
-  fi
-  chmod +x nezha-agent
-}
-
-# 运行客户端
-run() {
-  TLS=\${NEZHA_TLS:+'--tls'}
-  [[ ! \$PROCESS =~ nezha-agent && -e nezha-agent ]] && ./nezha-agent -s \${NEZHA_SERVER}:\${NEZHA_PORT} -p \${NEZHA_KEY} \${TLS} 2>&1 &
-}
-
-check_run
-check_variable
-download_agent
-run
-EOF
-}
-
-check_wget
-generate_config
-generate_nezha
-
-[ -e nezha.sh ] && bash nezha.sh
